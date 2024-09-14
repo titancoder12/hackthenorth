d06a90a1f3f565a2070038a85cbdf6c3a07e871c
Ananth Arunkumar <ananth2.718@gmail.com>
2024-09-14 19:06:57 -0400

    added new file to interface with backend

---
+++ frontend/getconversation.js
@@ -0,0 +1,11 @@
+async function startConversation(text1, text2, msgnum) {
+    var response = await fetch("http://localhost:8000/api/createconversation/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({prompt1: text1.value, prompt2: text2.value, nmessages: 5})
+    });
+    payload = await response.json();
+    return payload.conversation;
+}
